a8366c34c0c89e582aa45a706bbc5a09c15da8e5
Luiz Fernando Batista Morato <fernandomorato93@gmail.com>
2021-09-23 19:39:16 -0300

    Create DivisionTrick.cpp

---
+++ Math/DivisionTrick.cpp
@@ -0,0 +1,7 @@
+// by tfg
+// found at: https://github.com/tfg50/Competitive-Programming/blob/master/Biblioteca/Math/DivisionTrick.cpp
+// O(sqrt(n))
+for(int l = 1, r; l <= n; l = r + 1) {
+	r = n / (n / l);
+	// n / i has the same value for l <= i <= r
+}
