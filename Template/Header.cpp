b679a42fb0543591fb6a2214203ceabadc487bc1
Luiz Fernando Batista Morato <fernandomorato93@gmail.com>
2021-09-30 06:25:42 -0300

    Create Header.cpp

---
+++ Template/Header.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1e9 + 7;
+const int MAXN = 2e5 + 5;
+const ll INF = 1e18;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	return 0;
+}
