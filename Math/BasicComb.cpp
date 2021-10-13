4890e08de97bf83a496fe0907020283af5ca2023
Luiz Fernando Batista Morato <fernandomorato93@gmail.com>
2021-10-12 21:18:00 -0300

    Update BasicComb.cpp

--- Math/BasicComb.cpp
+++ Math/BasicComb.cpp
@@ -1,26 +1,28 @@
 // remember to change BOUNDS accordingly!!!
 
-Mint<> fat[MAXN], inv[MAXN];
+using T = int;
+
+T fat[MAXN], inv[MAXN];
 
 void setup() {
 	fat[0] = inv[0] = 1;
 	for (int i = 1; i < MAXN; i++) {
-		fat[i] = fat[i - 1] * i;
+		fat[i] = 1ll * fat[i - 1] * i % MOD;
 	}
 	inv[MAXN - 1] = fexp(fat[MAXN - 1], MOD - 2);
 	for (int i = MAXN - 2; i >= 1; i--) {
-		inv[i] = inv[i + 1] * (i + 1);
+		inv[i] = 1ll * inv[i + 1] * (i + 1) % MOD;
 	}
 }
 
 // C(n, k) = n choose k, number of ways to choose a set of k elements from a set of n elements
-Mint<> C(int n, int k) {
+T C(int n, int k) {
 	if (n < k || k < 0)
-		return Mint<>(0);
-	return fat[n] * inv[k] * inv[n - k];
+		return T(0);
+	return T(1ll * fat[n] * inv[k] % MOD * 1LL * inv[n - k] % MOD);
 }
 
 // C(n) = n-th Catalan number - number of valid parenthesis sequences of size 2 * n
-Mint<> C(int n) {
-	return C(2 * n, n) - C(2 * n, n + 1);
+T C(int n) {
+	return (MOD + C(2 * n, n) - C(2 * n, n + 1)) % MOD;
 }
