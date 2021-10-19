3549d22b713a749fcc27365a747f371d05accae8
Luiz Fernando Batista Morato <fernandomorato93@gmail.com>
2021-10-19 08:01:59 -0300

    Update ConvexHull.cpp

--- Math/Geometry/ConvexHull.cpp
+++ Math/Geometry/ConvexHull.cpp
@@ -1,15 +1,17 @@
 vector<PT> convexHull(vector<PT> &p, bool sorted = false) {
 	int n = (int) p.size(), k = 0;
-	if (!sorted) // may change this later, since p is a reference
+	if (!sorted)
 		sort(p.begin(), p.end());
 	vector<PT> h(2 * n);
+	// Upper-Hull
 	for (int i = 0; i < n; i++) {
-		while (k >= 2 && (p[i] - h[k - 2]) % (h[k - 1] - h[k - 2]) > 0)
+		while (k >= 2 && (p[i] - h[k - 2]) % (h[k - 1] - h[k - 2]) >= 0)
 			k--;
 		h[k++] = p[i];
 	}
+	// Lower-Hull
 	for (int i = n - 2, t = k + 1; i >= 0; i--) {
-		while (k >= t && (p[i] - h[k - 2]) % (h[k - 1] - h[k - 2]) > 0)
+		while (k >= t && (p[i] - h[k - 2]) % (h[k - 1] - h[k - 2]) >= 0)
 			k--;
 		h[k++] = p[i];
 	}
